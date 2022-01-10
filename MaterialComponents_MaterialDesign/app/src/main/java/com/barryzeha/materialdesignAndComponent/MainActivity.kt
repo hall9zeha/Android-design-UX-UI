253a736d0eccbded9a86281c4c3da62e133b5a0a
hall9zeha <vmwaretars@gmail.com>
2022-01-10 12:29:10 -0500

    Terminamos el ejemplo de Material.Components que es la última versión de diseño de android hasta la fecha, mientras theme.AppCompat era material design, hasta hace poco, solamente tomaremos el ejemplo de theme.components, por ser el más actual

---
+++ MaterialComponents_MaterialDesign/app/src/main/java/com/barryzeha/materialdesignAndComponent/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.barryzeha.materialdesignAndComponent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
