b9f9d31463cedc31ae1a812a5a4feb7196718cae
hall9zeha <vmwaretars@gmail.com>
2022-01-18 11:17:38 -0500

    Implementación de chips y groupChip(va por mi cuenta) terminada, una muy buena herramienta para usarla de etiquetas muy configurable, lo necesario para mis fines

--- MaterialComponents_MaterialDesign/app/src/main/java/com/barryzeha/materialdesignAndComponent/fragments/CardFragment.kt
+++ MaterialComponents_MaterialDesign/app/src/main/java/com/barryzeha/materialdesignAndComponent/fragments/CardFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CompoundButton
+import android.widget.Toast
 import com.barryzeha.materialdesignAndComponent.R
 import com.barryzeha.materialdesignAndComponent.databinding.FragmentCardBinding
 import com.barryzeha.materialdesignAndComponent.utils.Component
@@ -14,6 +16,8 @@ import com.bumptech.glide.RequestBuilder
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.Request
 import com.bumptech.glide.request.RequestOptions
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
@@ -45,6 +49,45 @@ class CardFragment : Fragment() {
         Glide.with(requireActivity()).load("https://w.wallhaven.cc/full/1k/wallhaven-1kl36v.png")
             .apply(request)
             .into(bind.imgLargeCard)
+
+        //rellenando los chips dinámicamente a un groupchip
+
+        val array:IntArray = intArrayOf(R.string.card_chip_demo,R.string.card_chip_demo,R.string.card_chip_demo,R.string.card_chip_demo)
+        var id=0
+        for(text:Int in array){
+            id++
+            var chip= Chip(bind.chipGroupLarge.context)
+            chip.id=id
+            /*chip.isCheckable=true*/
+            chip.setOnClickListener { Toast.makeText(activity, chip.id.toString(), Toast.LENGTH_SHORT).show() }
+            chip.setText(text)
+            bind.chipGroupLarge.addView(chip)
+        }
+        bind.chipIndividual.setOnClickListener { Toast.makeText(activity, bind.chipIndividual.text, Toast.LENGTH_SHORT).show() }
+      bind.chipGroupLarge.setOnCheckedChangeListener { group, checkedId ->
+          var chip=bind.chipGroupLarge.findViewById<Chip>(checkedId)
+            if(chip.isChecked) {
+                Toast.makeText(
+                    activity,
+                    chip.text.toString(),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+
+      }
+       bind.chipCheckable.setOnCheckedChangeListener { p0, p1 ->
+           if(p1){
+               Toast.makeText(activity, "checked enable", Toast.LENGTH_SHORT).show()
+           }
+           else{
+               Toast.makeText(activity, "checked disable", Toast.LENGTH_SHORT).show()
+           }
+
+       }
+        bind.chipClose.setOnCloseIconClickListener {
+            bind.chipClose.visibility=View.GONE
+        }
         return bind.root
     }
 
